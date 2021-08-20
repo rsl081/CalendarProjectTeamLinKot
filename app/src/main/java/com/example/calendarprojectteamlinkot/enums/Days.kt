eebcbf72b3a82045c6475e5be5d55572446c4938
rsl081 <53944109+rsl081@users.noreply.github.com>
2021-08-20 11:53:26 +0800

    MeTask

--- app/src/main/java/com/example/calendarprojectteamlinkot/enums/Days.kt
+++
@@ -1,5 +0,0 @@
-package com.example.calendarprojectteamlinkot.enums
-
-enum class Days {
-    SUN, MON, TUE, WED, THU, FRI, SAT
-}
--- app/src/main/java/com/example/calendarprojectteamlinkot/enums/Month.kt
+++
@@ -1,7 +0,0 @@
-package com.example.calendarprojectteamlinkot.enums
-
-enum class Month {
-    January, February, March, April,
-    May, June, July, August,
-    September, October, November, December,;
-}
