d3e905ac3fd978159fff407f0381e8977fd59e28
rsl081 <53944109+rsl081@users.noreply.github.com>
2021-08-24 17:03:08 +0800

    SiginActivity Done

--- app/src/main/java/com/example/calendarprojectteamlinkot/view/MainActivity.kt
+++ app/src/main/java/com/example/calendarprojectteamlinkot/view/MainActivity.kt
@@ -2,18 +2,26 @@ package com.example.calendarprojectteamlinkot.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.example.calendarprojectteamlinkot.R
 
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-
+    override fun onBackPressed()
+    {
+//        if(drawer_layout.isDrawerOpen(GravityCompat.START)){
+//            drawer_layout.closeDrawer(GravityCompat.START)
+//        }else{
+//            doubleBacktoExit()
+//        }
+        super.onBackPressed()
     }
 
 }
