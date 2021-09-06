659f9e39c3571a14c538dcaeb2a45591caad10bf
Gen2102 <genesis@s2dioapps.com>
2021-09-06 20:40:40 +0800

    gen/registrationSlide

---
+++ app/src/main/java/com/example/calendarprojectteamlinkot/view/PasswordActivity.kt
@@ -0,0 +1,21 @@
+package com.example.calendarprojectteamlinkot.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.calendarprojectteamlinkot.R
+import kotlinx.android.synthetic.main.activity_password.*
+
+class PasswordActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password)
+
+        btn_back.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
+        }
+    }
+}
