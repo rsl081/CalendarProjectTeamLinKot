1efc605f6680a74098f584d0af50d7fed1350b93
Gen2102 <genesis@s2dioapps.com>
2021-09-09 14:41:16 +0800

    gen/confirmpass

--- app/src/main/java/com/example/calendarprojectteamlinkot/view/VerificationActivity.kt
+++ app/src/main/java/com/example/calendarprojectteamlinkot/view/VerificationActivity.kt
@@ -1,12 +1,22 @@
 package com.example.calendarprojectteamlinkot.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.calendarprojectteamlinkot.R
+import kotlinx.android.synthetic.main.activity_forget_pass.*
+import kotlinx.android.synthetic.main.activity_verification.*
 
 class VerificationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verification)
+
+        btn_verify.setOnClickListener {
+            val intent = Intent(this, ChangePassActivity::class.java)
+
+            startActivity(intent)
+
+        }
     }
 }
